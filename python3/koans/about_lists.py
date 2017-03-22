7d1f388a0f9bec1c8ef44f5b9c1c9b8d889c93da
kimegitee <kimegitee@gmail.com>
2017-03-22 21:16:25 +0700

    Remove element at index supplied as argument to pop()

--- python3/koans/about_lists.py
+++ python3/koans/about_lists.py
@@ -83,8 +83,8 @@ class AboutLists(Koan):
         self.assertEqual([10, 20, 30, 40], stack)
 
         popped_value = stack.pop(1)
-        self.assertEqual(__, popped_value)
-        self.assertEqual(__, stack)
+        self.assertEqual(20, popped_value)
+        self.assertEqual([10, 30, 40], stack)
 
         # Notice that there is a "pop" but no "push" in python?
 
