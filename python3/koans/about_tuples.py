ff0e0c4e47fe1474b607671ffa45e9612a836d8b
kimegitee <kimegitee@gmail.com>
2017-03-23 15:40:41 +0700

    Create a tuple from a string

--- python3/koans/about_tuples.py
+++ python3/koans/about_tuples.py
@@ -42,7 +42,7 @@ class AboutTuples(Koan):
         self.assertEqual(tuple(["Hello comma!"]), ("Hello comma!", ))
 
     def test_tuple_constructor_can_be_surprising(self):
-        self.assertEqual(__, tuple("Surprise!"))
+        self.assertEqual(('S', 'u', 'r', 'p', 'r', 'i', 's', 'e', '!'), tuple("Surprise!"))
 
     def test_creating_empty_tuples(self):
         self.assertEqual(__ , ())
