55ab3cdab849fda7cf28a46515a21af24a172ee8
kimegitee <kimegitee@gmail.com>
2017-03-23 15:08:47 +0700

    Test if tuples support assignment

--- python3/koans/about_tuples.py
+++ python3/koans/about_tuples.py
@@ -19,7 +19,7 @@ class AboutTuples(Koan):
         # Note, assertRegex() uses regular expression pattern matching,
         # so you don't have to copy the whole message.
 
-        self.assertRegex(msg, __)
+        self.assertRegex(msg, "support")
 
     def test_tuples_are_immutable_so_appending_is_not_possible(self):
         count_of_three =  (1, 2, 5)
